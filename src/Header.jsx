7a9ac6a44d3432cd21ab560fa6c57d6d324ac597
alejandrojokmanovich <alejandrojokmanovich@Airdealmanovich.fibertel.com.ar>
2021-07-30 21:25:31 -0300

    react wavify

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import Wave from "react-wavify";
 import video from "./resouces/vide.mp4"
-import w1 from "./resouces/w1.png"
-import w2 from "./resouces/w2.png"
-import w3 from "./resouces/w3.png"
 
 function Header() {
     return (
         <div>
             <div id="intro"className="p-absolute header-container gradient-header  ">
+                <div className="clip-path gradient-header"></div>
                 <div className="black"></div>
+                
                 <div className="video-contenedor">
                 <video autoplay loop muted className="video"  >
                         <source autoplay src="" type="video/mp4"  />
@@ -20,15 +20,42 @@ function Header() {
                 <h1 className="poppins-font txt-slogan">Innovation is always present at EnterTEK </h1>
                 <button className="button poppins-font " ><a className="text-decoration-none" href="#contact">Contact us</a></button> 
             </div>
-            <div >
+            <Wave className="wave3"
+            fill="black"
+            paused={false}
+            options={{
+                height: 25,
+                amplitude: 60,
+                speed: 0.20,
+                points: 7
+              }}
+            />
+              <Wave className="wave2"
+            fill="white"
+            paused={false}
+            options={{
+                height: 55,
+                amplitude: 60,
+                speed: 0.20,
+                points: 3
+              }}
+            />
+            <Wave className="wave"
+            fill="white"
+            paused={false}
+            options={{
+                height: 35,
+                amplitude: 50,
+                speed: 0.25,
+                points: 5
+              }}
+            />
+          
+           
             
-                <div className="line-3">
-                    <img className="waves wave3" src={w3} alt="" />
-                </div>
-            </div>
-
         </div>
     )
 }
 
 export default Header
+
